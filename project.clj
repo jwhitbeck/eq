10f6f7f67005c238b8af38a259e5568fff0bed92
John Whitbeck <john@whitbeck.fr>
2015-08-28 22:54:03 -0700

    Upgrade to clojurescript 1.7

--- project.clj
+++ project.clj
@@ -5,13 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/jwhitbeck/eq"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [org.clojure/tools.cli "0.3.1"]]
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/tools.cli "0.3.3"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
   :source-paths ["src"]
   :hooks [leiningen.cljsbuild]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :cljsbuild {:builds [{:source-paths ["src"]
                         :notify-command ["./post-build.sh"]
                         :compiler {:target :nodejs
