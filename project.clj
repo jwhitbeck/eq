5fee2c0b564fcfea50e63be6351722c0dd8307a4
John Whitbeck <john@whitbeck.fr>
2016-04-28 00:06:36 -0700

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
         :url "https://github.com/jwhitbeck/eq"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.cli "0.3.3"]]
   :plugins [[lein-cljsbuild "1.1.0"]]
   :source-paths ["src"]
   :hooks [leiningen.cljsbuild]
   :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
-                                  [org.clojure/tools.nrepl "0.2.11"]]
+                                  [org.clojure/tools.nrepl "0.2.12"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :cljsbuild {:builds [{:source-paths ["src"]
                         :notify-command ["./post-build.sh"]
